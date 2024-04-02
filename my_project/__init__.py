16023751aed3452cd626dc5e2f98b5178e4ec919
Michael Brunner <brunner@cadwork.swiss>
2024-04-02 09:53:00 +0200

    Add asyncio support and refactor code

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -1,7 +1,9 @@
 import math
+import attribute_controller as ac
 import element_controller as ec
 import geometry_controller as gc
 import cadwork
+import asyncio
 
 def my_decorator(func):
     def wrapper():
@@ -33,21 +35,25 @@ class WhatIsHappening:
 
 def matrix_multiply(matrix, point):
     result = cadwork.point_3d(0, 0, 0)
+    point_coord_count = 3
     for i in range(len(matrix)):
-        for j in range(3): # len(point)
+        for j in range(point_coord_count):
             result[i] += matrix[i][j] * point[j]
     # result = [sum(m*p for m, p in zip(row, point)) for row in matrix]
     return result
 
 
-
-
-if __name__ == "__main__":
+async def main():
     print("Hello world")
     say_whee()
-    my_cls = WhatIsHappening()
 
     element_ids = ec.get_active_identifiable_element_ids()
+    if len(element_ids) < 1:
+        print("No elements selected")
+        return
+    if ac.get_element_type(*element_ids).is_surface() == False:
+        print("Selected element is not a surface")
+        return
 
     # face vertices
     # points = [
@@ -56,7 +62,7 @@ if __name__ == "__main__":
     #     [1000.000000, 1000.000000, 0.000000],
     #     [1000.000000, 0.000000, 0.000000]
     # ]
-    points = gc.get_element_vertices(*element_ids)
+    points = await gc.get_element_vertices(*element_ids)
     angle = math.radians(90)
 
     # rotate around y-axis
@@ -78,3 +84,19 @@ if __name__ == "__main__":
     print(rotated_points)
 
     ec.create_surface(rotated_points)
+
+async def sleep_print(n): 
+    print(f"I wait {n} seconds ...") 
+    await asyncio.sleep(n) 
+    print(f"... i am back (after {n} seconds)!")
+    return n
+
+async def program():
+    task1 = asyncio.create_task(sleep_print(2))
+    task2 = asyncio.create_task(sleep_print(3))
+    task3 = asyncio.create_task(sleep_print(5))
+    main_task = asyncio.create_task(main())
+    await asyncio.gather(task1, task2, task3, main_task)
+
+if __name__ == "__main__":
+    asyncio.run(program())
